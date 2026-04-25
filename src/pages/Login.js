2823181d1361b0c4fa985e42cc46f920c371b597
SaiMadhavSunkara <sunkarasaimadhav@gmail.com>
2026-04-26 03:01:55 +0530

    fixed navigate usage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
   const handleLogin = async (e) => {
     e.preventDefault();
 
